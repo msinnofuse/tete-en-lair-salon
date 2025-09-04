7a9aadf3a18f51961f18ccb081bdcda36d6a5cf8
msinnofuse <michela.schorta@innofuse.swiss>
2025-09-04 08:06:11 +0200

    gitpages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://msinnofuse.github.io',
+  base: 'tete-en-lair-salon',
   vite: {
     plugins: [tailwindcss()]
   }
